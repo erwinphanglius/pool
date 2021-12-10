e0fb9a4cac01a1fe7cb18c9ebc61f5cea41bd975
erwinphanglius <erwinphanglius10@gmail.com>
2021-12-11 01:09:46 +0700

    wrap into 1 singel mapping

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,13 +3,14 @@ import {
   PoolFactory,
   PoolCreation
 } from "../generated/PoolFactory/PoolFactory"
+import { FundPool } from "../generated/templates/MetaversepadTemplate/Metaversepad"
 import { PoolCreationEntity, MetaversepadPool } from "../generated/schema"
 
 export function handlePoolCreation(event: PoolCreation): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
   let entity = PoolCreationEntity.load(event.transaction.hash.toHex())
-  let poolEntity = MetaversepadPool.load(event.params.poolAddress.toHexString())
+  // let poolEntity = MetaversepadPool.load(event.params.poolAddress.toHexString())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
@@ -20,12 +21,12 @@ export function handlePoolCreation(event: PoolCreation): void {
     // entity.count = BigInt.fromI32(0)
   }
 
-  if (!poolEntity) {
-    poolEntity = new MetaversepadPool(event.params.poolAddress.toHexString())
+  // if (!poolEntity) {
+  //   poolEntity = new MetaversepadPool(event.params.poolAddress.toHexString())
 
     // Entity fields can be set using simple assignments
     // entity.count = BigInt.fromI32(0)
-  }
+  // }
   // BigInt and BigDecimal math are supported
   // entity.count = entity.count + BigInt.fromI32(1)
 
@@ -57,4 +58,34 @@ export function handlePoolCreation(event: PoolCreation): void {
   // - contract.owner(...)
 }
 
-// export function handlePoolCreation(event: PoolCreation): void {}
+export function handleFundPool(eventPoolCreation: PoolCreation, eventPoolInfo: FundPool): void {
+  // let poolCreationEntity = PoolCreationEntity.load(eventPoolCreation.transaction.hash.toHex())
+  let poolEntity = MetaversepadPool.load(eventPoolCreation.params.poolAddress.toHexString())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!poolEntity) {
+    poolEntity = new MetaversepadPool(eventPoolCreation.params.poolAddress.toHexString())
+
+    // Entity fields can be set using simple assignments
+    // entity.count = BigInt.fromI32(0)
+  }
+
+  if (!poolEntity) {
+    poolEntity = new MetaversepadPool(eventPoolCreation.params.poolAddress.toHexString())
+
+    // Entity fields can be set using simple assignments
+    // entity.count = BigInt.fromI32(0)
+  }
+  // BigInt and BigDecimal math are supported
+  // entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  // entity.previousOwner = event.params.previousOwner
+  // entity.newOwner = event.params.newOwner
+  poolEntity.address = eventPoolInfo.params.initiator
+  poolEntity.balance = eventPoolInfo.params.value
+
+  // Entities can be written to the store with `.save()`
+  poolEntity.save()
+}
